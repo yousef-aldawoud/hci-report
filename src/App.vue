65e1ea3037362b38931a62c3c40b450e53d3160f
Yousef <yousef.l.t.aldawoud@gmail.com>
2019-10-15 04:24:31 +1300

    Made small changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+     <v-app-bar app class="navbar">
+
+      <v-app-bar-nav-icon @click="drawer = ! drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>HCI Report</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text>Team members</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-navigation-drawer
+          class="deep-purple accent-4"
+          dark
+          app
+          v-model="drawer"
+        >
+        <v-list>
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+            :href="item.link"
+          >
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+      </v-navigation-drawer>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data: () => ({
+    drawer:true,
+     items: [
+          { title: 'Home', link: 'title' },
+          { title: 'About', link: 'style' },
+        ]
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style  scoped>
+.navbar{
+  max-height :70px;
 }
 </style>
---
+++ src/components/Report.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout class="mx-2 px-2">
+      <v-flex xs12>
+
+      <div id="#title">
+        <h1>Hello world</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque delectus autem quibusdam quod. Odio, asperiores. Sint odio vel sapiente natus quae eos voluptate perspiciatis? Excepturi ducimus quasi tempora facere alias?</p>
+      </div>
+      <div id="#style">
+        <h1>Hello world</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque delectus autem quibusdam quod. Odio, asperiores. Sint odio vel sapiente natus quae eos voluptate perspiciatis? Excepturi ducimus quasi tempora facere alias?</p>
+      </div>
+      </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
